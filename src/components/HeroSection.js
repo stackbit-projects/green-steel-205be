f92039d3f022faeb758b63fce44433fdd192d395
Stackbit Code Editor <projects@stackbit.com>
2021-03-17 19:54:51 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,7 +4,7 @@ import _ from 'lodash';
 
 import {classNames, toStyleObj, withPrefix} from '../utils';
 import SectionActions from './SectionActions';
-import classes from './BackgroundVideo.module.css';
+import classes from './';
 
 export default class HeroSection extends React.Component {
  
