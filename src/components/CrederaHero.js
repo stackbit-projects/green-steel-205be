abe5fc1ade08502b6ff2fdc21a7b751e7dfd13fd
Stackbit Code Editor <projects@stackbit.com>
2021-03-17 15:01:09 +0000

    CrederaHero.js: changed

--- src/components/CrederaHero.js
+++ src/components/CrederaHero.js
@@ -9,7 +9,9 @@ export default class HeroSection extends React.Component {
     render() {
         <div id="shopify-section-commerical-hero" class="shopify-section credera-section home-hero">
             <div class="video-hero hero-container">
-}
+                <video poster="//cdn.shopify.com/s/files/1/2422/9487/files/Connect_EX-7s_-_Lifestyle_4-2048x1367-8d625cc_1024x768-2.jpg?v=1614883176" autoplay="" muted="" loop="" playsinline="" id="video-banner2"> 
+                <source src="https://cdn.shopify.com/s/files/1/2422/9487/files/EX7S_Gym_30sec_Cut2.mp4?v=1603803063" type="video/mp4" />
+                </video> 
 
         </div>
 
