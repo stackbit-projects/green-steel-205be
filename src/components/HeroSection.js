16da4fcd3da475e662cb6df1cb09c3e8965e62f3
Stackbit Code Editor <projects@stackbit.com>
2021-03-17 15:10:38 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,6 +7,7 @@ import SectionActions from './SectionActions';
 
 export default class HeroSection extends React.Component {
     render() {
+        return()
         <div id="shopify-section-commerical-hero" class="shopify-section credera-section home-hero">
             <div class="video-hero hero-container">
                 <video poster="//cdn.shopify.com/s/files/1/2422/9487/files/Connect_EX-7s_-_Lifestyle_4-2048x1367-8d625cc_1024x768-2.jpg?v=1614883176" autoplay="" muted="" loop="" playsinline="" id="video-banner2"> 
