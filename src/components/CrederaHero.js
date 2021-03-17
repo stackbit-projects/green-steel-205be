4602fe55ba1e93eeed6f32846712d145c06d19a6
Stackbit Code Editor <projects@stackbit.com>
2021-03-17 14:55:08 +0000

    CrederaHero.js: changed

--- src/components/CrederaHero.js
+++ src/components/CrederaHero.js
@@ -1,3 +1,13 @@
+
+import React from 'react';
+import _ from 'lodash';
+
+import {classNames, toStyleObj, withPrefix} from '../utils';
+import SectionActions from './SectionActions';
+
+export default class HeroSection extends React.Component {
+    render() {
+
 <div id="shopify-section-commerical-hero" class="shopify-section credera-section home-hero">
 
 
