7e9c17eaab0c274f3b69b0a8a2fec71d82558bea
Stackbit Code Editor <projects@stackbit.com>
2021-03-17 15:25:59 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -14,7 +14,7 @@ export default class HeroSection extends React.Component {
                 <video poster="//cdn.shopify.com/s/files/1/2422/9487/files/Connect_EX-7s_-_Lifestyle_4-2048x1367-8d625cc_1024x768-2.jpg?v=1614883176" autoplay="" muted="" loop="" playsinline="" id="video-banner2"> 
                 <source src="https://cdn.shopify.com/s/files/1/2422/9487/files/EX7S_Gym_30sec_Cut2.mp4?v=1603803063" type="video/mp4" />
                 </video>
-                <div  style="background-color: rgba(0, 0, 0, 0.4);"> 
+                <div> 
                     <div> <h1>Meet your fitness goals with Echelon.</h1> 
                         <p>Echelon Commercial Offerings</p>
                     </div>
